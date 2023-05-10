06264fa8fb5f456c3f4f6fc367607ff3660468d2
andrsmllr <andrsmllr@bananatronics.org>
2023-05-10 19:39:37 +0200

    Refactor Settings.py

--- Settings.py
+++ Settings.py
@@ -5,42 +5,54 @@ __author__ = "iEpic"
 __email__ = "epicunknown@gmail.com"
 """
 
-import os
 import json
+from json import JSONDecodeError
+
+
+default_settings = {
+    "allowToResumeDownloads": True,
+    "checkForUpdates": True,
+    "checkIfFileIsAlreadyDownloaded": True,
+    "defaultOutputLocation": False,
+    "downloadsDatabaseLocation": "./database.p",
+    "episodePadding": 2,
+    "includeShowDesc": True,
+    "saveDownloadLocation": True,
+    "saveFormat": "{show}-S{season}E{episode}-{desc}",
+    "seasonPadding": 2,
+    "useKnownDownloadLocation": True
+}
 
 
 class Settings:
     """Object that holds user settings"""
 
-    def __init__(self):
-        self.loaded_settings = {}
-        # Does settings.json file exist?
-        if os.path.exists('settings.json'):
-            # Load up the settings
-            with open('settings.json') as file:
-                self.loaded_settings = json.load(file)
-                print('Settings Loaded.')
-        else:
-            # Lets create a new settings file
-            self.loaded_settings['includeShowDesc'] = True
-            self.loaded_settings['saveFormat'] = '{show}-S{season}E{episode}-{desc}'
-            self.loaded_settings['episodePadding'] = 2
-            self.loaded_settings['seasonPadding'] = 2
-            self.loaded_settings['defaultOutputLocation'] = False
-            self.loaded_settings['saveDownloadLocation'] = True
-            self.loaded_settings['useKnownDownloadLocation'] = True
-            self.loaded_settings['checkIfFileIsAlreadyDownloaded'] = True
-            self.loaded_settings['downloadsDatabaseLocation'] = ".{os_path}{file_name}".format(os_path=os.sep, file_name='database.p')
-            self.loaded_settings['allowToResumeDownloads'] = True
-            self.loaded_settings['checkForUpdates'] = True
-            
-            file = open('settings.json', 'w')
-            file.write(json.dumps(self.loaded_settings, indent=4, sort_keys=True))
-            file.close()
-            print('Settings have been created!')
+    def __init__(self, settings_file):
+        self._settings = {}
+        self._settings_file = settings_file
+
+        try:
+            with open(self._settings_file) as file:
+                self._settings = json.load(file)
+                print(f"Settings loaded successfuly from {self._settings_file}")
+        except FileNotFoundError:
+            print(f"Failed to load {self._settings_file}, creating {self._settings_file} with default settings")
+            self._settings = default_settings
+            self._write_settings_to_file()
+        except (JSONDecodeError) as error:
+            print(f"Failed to open {self._settings_file} as JSON", str(error))
+            raise error
+
+    def _write_settings_to_file(self):
+        try:
+            with open(self._settings_file, 'w') as file:
+                file.write(json.dumps(self._settings, indent=4, sort_keys=True))
+            print(f"Settings file {self._settings_file} written successfully")
+        except Exception:
+            print(f"Failed to open or write file {self._settings_file}")
 
     def get_setting(self, setting_name):
         """Get the value for a setting"""
 
-        if setting_name in self.loaded_settings:
-            return self.loaded_settings[setting_name]
+        if setting_name in self._settings:
+            return self._settings[setting_name]
